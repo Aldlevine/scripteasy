c2dd7ceaae135fd4dadc3fa63b9ad41dd32e31c8
Aaron Levine <ald.levine@gmail.com>
2017-11-17 20:06:52 -0500

    feat(cli): Add option parsing and `--help` option

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,62 @@
 #!/usr/bin/env node
 
 const path = require('path');
-// const minimist = require('minimist');
 const ST = require('./scripteasy');
 
 /** @type {Array<string>} */
 const argv = process.argv.slice(2);
 
+/** @type {number} */
+const commandIdx = argv.findIndex((arg) => arg.charAt(0) !== '-');
+
+/** @type {Array<string>} */
+const easyArgv = commandIdx >= 0 ? argv.slice(0, commandIdx) : argv.slice();
+
+/** @type {Array<string>} */
+const commandsArgv = argv.slice(commandIdx);
+
 /** @type {Array<Array<string>>} */
-const commands = argv.join(' ').split(' -- ').map(s => s.trim().split(' '));
+const commands = commandsArgv.join(' ')
+  .split(' -- ')
+  .filter(s => s.length)
+  .map(s => s.trim().split(' '));
 
 /** @type {object | string} */
 const {scripteasy} = require(path.join(process.cwd(), 'package.json'));
 
 let st;
+
+/**
+ * Show the help message and exit
+ */
+function help ()
+{
+  console.log(
+//------------------------------------------------------------------------------
+`scripteasy ${require('../package.json').version}
+
+Usage:
+--------
+ez [opts] [[<script>] [args] [--]...]
+
+
+Options:
+--------
+--help -h                 Show this message`
+//------------------------------------------------------------------------------
+  );
+  process.exit(0);
+}
+
+if (easyArgv.length) {
+  const minimist = require('minimist');
+  const argv = minimist(easyArgv);
+  if (argv.help || argv.h) help();
+}
+
+if (commands.length == 0) {
+  help();
+}
 
 if (typeof scripteasy === 'string') {
   st = ST.fromFile(scripteasy);
